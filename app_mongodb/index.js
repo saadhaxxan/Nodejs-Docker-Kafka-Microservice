5a573befd556f9673f270d2e348c8628b6e9f64d
Saad Hassan <saad.haxxan786@gmail.com>
2022-04-03 03:42:40 -0700

    added kafka

--- app_mongodb/index.js
+++ app_mongodb/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const env = require('dotenv').config();
 
 app = express();
+
+const mongo_uri  = process.env.MONGODB_URI;
+
